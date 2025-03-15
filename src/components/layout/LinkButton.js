4539316e372c5e8de4ff90128fece8edf70ce590
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-15 14:28:20 -0300

    Criando a Home

---
+++ src/components/layout/LinkButton.js
@@ -0,0 +1,10 @@
+import  { Link } from 'react-router-dom';
+import styles from './LinkButton.module.css';
+function Linkbutton({to, text}){
+    return (
+        <Link  className={styles.btn} to={to}>
+        {text}
+        </Link>
+    )
+}
+export default Linkbutton;
