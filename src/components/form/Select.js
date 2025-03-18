a7de53ce75a3bea81cb7a26c037e22b2d6ccad34
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-18 19:18:59 -0300

    Conectando com API pelo React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,13 +1,18 @@
 import style from './Select.module.css';
 
-function Select({ text, name, options, handleOnchange, value}) {
- return (  
-     <div className={style.form_control}>
-        <label htmlFor={name}>{text}:</label>
-        <select name={name} id={name}>
-        <option> Selecione uma opção</option>
-        </select>
-    </div>
- )
+function Select({ text, name, options, handleOnchange, value }) {
+    return (
+        <div className={style.form_control}>
+            <label htmlFor={name}>{text}:</label>
+            <select name={name} id={name} onChange={handleOnchange} value={value}>
+                <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option key={option.id} value={option.id}>
+                        {option.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 }
-export default Select
+export default Select;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,33 +1,56 @@
+import React, { useEffect, useState } from 'react';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css';
 
+function ProjectForm({ btnText }) {
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(''); // Adicione este estado
 
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
 
-function  ProjectForm({btnText}){
+    const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
     return (
         <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
 
-          <Input
-           type = "text"
-           text = "Nome do projeto" 
-           name = "name" 
-           placeholder = "Insira o nome do projeto"/>
-         
-         <Input 
-         type="number"
-         text="Orçamento do projeto"
-         name="budget"
-         placeholder="Insira o orçamento total"
-        />
-        <Select 
-            name="category_id"
-            text="Selecione a categoria"
-            options />    
+            <Input
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento total"
+            />
+            <Select
+                name="category_id"
+                text="Selecione a categoria"
+                options={categories}
+                handleOnchange={handleCategoryChange} // Passe a função de mudança
+                value={selectedCategory}
+            />
 
-         <SubmitButton text={btnText} />
+            <SubmitButton text={btnText} />
         </form>
-    )
+    );
 }
 export default ProjectForm;
