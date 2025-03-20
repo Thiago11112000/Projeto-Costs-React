1670cd261c6de6a287fb38834ee01cd5d6714b7f
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-03-20 11:29:43 -0300

    Criando Dashboard

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg';
-import Linkbutton from '../layout/LinkButton';
+import LinkButton from '../layout/LinkButton';
 function Home(){
 return  (
     <section className={styles.home_container}>
     <h1>Bem-Vindo ao <span>Costs </span></h1>
     <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-    <Linkbutton to="/newproject" text="Criar Projeto"/>
+    <LinkButton to="/newproject" text="Criar Projeto"/>
     <img src={savings} alt="Costs"/>
     </section>
 )
